ee13981d0d764f2d88a072f7adcf50b313333b13
Franco Robles <franco_robles94@outlook.com>
2023-02-24 13:28:56 -0300

    register and login completado (falta logout)

---
+++ src/main/java/com/pipepiper/socialnetwork/controllers/AuthController.java
@@ -0,0 +1,92 @@
+package com.pipepiper.socialnetwork.controllers;
+
+
+import com.pipepiper.socialnetwork.models.User;
+import com.pipepiper.socialnetwork.repositorys.UserRepository;
+import com.pipepiper.socialnetwork.security.jwt.JwtTokenUtil;
+import com.pipepiper.socialnetwork.security.payload.JwtResponse;
+import com.pipepiper.socialnetwork.security.payload.LoginRequest;
+import com.pipepiper.socialnetwork.security.payload.MessageResponse;
+import com.pipepiper.socialnetwork.security.payload.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controlador para llevar a cabo la autenticación utilizando JWT
+ *
+ * Se utiliza AuthenticationManager para autenticar las credenciales que son el
+ * usuario y password que llegan por POST en el cuerpo de la petición
+ *
+ * Si las credenciales son válidas se genera un token JWT como respuesta
+ */
+// @CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public AuthController(AuthenticationManager authManager,
+                          UserRepository userRepository,
+                          PasswordEncoder encoder,
+                          JwtTokenUtil jwtTokenUtil){
+        this.authManager = authManager;
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest){
+
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtTokenUtil.generateJwtToken(authentication);
+
+        // UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<MessageResponse> register(@RequestBody RegisterRequest signUpRequest) {
+
+        // check if the user already exists
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        // check if the Email already exists
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                             signUpRequest.getEmail(),
+                             encoder.encode(signUpRequest.getPassword()));
+        userRepository.save(user);
+
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(){
+        return null;
+    }
+}
