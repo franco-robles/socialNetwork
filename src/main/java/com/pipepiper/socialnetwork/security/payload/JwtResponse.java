97f85396d736a2979b45e1aa0a1ab5d23fd9c968
Franco Robles <franco_robles94@outlook.com>
2023-02-24 19:54:51 -0300

    solo tiene nombre

--- src/main/java/com/pipepiper/socialnetwork/security/payload/JwtResponse.java
+++ src/main/java/com/pipepiper/socialnetwork/security/payload/JwtResponse.java
@@ -3,18 +3,27 @@ package com.pipepiper.socialnetwork.security.payload;
 import io.jsonwebtoken.Jwt;
 
 public class JwtResponse {
-    private String token;
+    private String username;
+    //private String email;
 
-
-    public JwtResponse(){}
-
-    public JwtResponse(String token){ this.token = token;}
-
-    public String getToken() {
-        return token;
+    public JwtResponse(String username ) {
+        this.username = username;
+       // this.email = email;
+    }
+    public JwtResponse() {}
+    public String getUsername() {
+        return username;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+   // public String getEmail() {
+   //     return email;
+   // }
+//
+   // public void setEmail(String email) {
+   //     this.email = email;
+   // }
 }
