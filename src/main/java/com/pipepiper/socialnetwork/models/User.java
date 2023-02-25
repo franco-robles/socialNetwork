c17d0a62b7389e61bca81fe8cca0d4349904bb0f
Franco Robles <franco_robles94@outlook.com>
2023-02-24 22:00:54 -0300

    pequeños cambion en los atrubutos

--- src/main/java/com/pipepiper/socialnetwork/models/User.java
+++ src/main/java/com/pipepiper/socialnetwork/models/User.java
@@ -3,68 +3,56 @@ package com.pipepiper.socialnetwork.models;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
 
 @Entity
 @Table(name = "Users")
 public class User {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String lastName;
-    private Long age;
+    private String username;
     private String email;
-    //private Date date;
+    private String password ;
 
+
+    //contructor, setters and getters
     public User() {
     }
 
-    public User(Long id, String name, String lastName, Long age, String email) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
+    public User(String username, String email, String password) {
+        this.username = username;
         this.email = email;
+        this.password = password;
     }
-
 
     public Long getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public void setAge(Long age) {
-        this.age = age;
-    }
+    public String getPassword() {return password; }
 
     public String getEmail() {
-        return email;
+            return email;
+        }
+        
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void setPassword(String password) {this.password = password; }
+
+
 }
